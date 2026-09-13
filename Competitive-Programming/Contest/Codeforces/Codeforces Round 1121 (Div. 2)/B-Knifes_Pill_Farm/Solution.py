b22e07d3788004e26aa836f305144973e89ce1c7
Abhhiiissshhek <abhipraj1301@gmail.com>
2026-09-13 23:49:03 +0530

    Feat: uploaded the B question

--- Competitive-Programming/Contest/Codeforces/Codeforces Round 1121 (Div. 2)/B-Knifes_Pill_Farm/Solution.py
+++ Competitive-Programming/Contest/Codeforces/Codeforces Round 1121 (Div. 2)/B-Knifes_Pill_Farm/Solution.py
@@ -1,1 +1,30 @@
-p
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    if m == 1:
+        print(max(a))
+        continue
+
+    heap = []
+    total = 0
+    answer = -10**30
+
+    for x in a:
+        if len(heap) == m - 1:
+            answer = max(answer, m * x - total)
+
+        heapq.heappush(heap, -x)
+        total += x
+
+        if len(heap) > m - 1:
+            total += heapq.heappop(heap)
+
+    print(answer)
